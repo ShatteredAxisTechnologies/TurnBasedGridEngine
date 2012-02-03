5f1d634f871a119d841333a63c106072c43f4c29
Aaron Jones <r.aaron.jones@gmail.com>
2012-02-03 10:41:04 -0800

    Initial Commit

---
+++ FrickinLaserBeamsPOC/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  FrickinLaserBeamsPOC
+//
+//  Created by Aaron Jones on 2/3/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
